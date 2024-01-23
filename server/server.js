27478ee62158676b9ba1174232d2ded6690b20cb
Bin Li <northblue@gmail.com>
2024-01-23 14:18:16 +1100

    Removed server.js and and updated the package.json because the requirement from vercel

--- server/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/api', (req, res) => {
-    console.log('hit server /api')
-    res.json({ message: 'Hello from server!' })
-})
-
-app.listen(5000, () => console.log('Server started on port 5000'))
