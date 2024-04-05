a62661cf8d901985ea0785b0e30eb4df60a23c9d
Bin Li <northblue@gmail.com>
2024-04-05 22:08:00 +1100

    Added type write effect on home section

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
 import { Link } from 'react-scroll';
+import Typewriter from 'typewriter-effect';
 import Portrait from '../assets/portrait.jpeg';
 
 function Home() {
@@ -11,16 +12,28 @@ function Home() {
 
             <div className='max-w-[1000px] w-full grid sm:grid-cols-3 gap-8 px-4'>
                 <div className='text-[color:var(--primary-text-color-normal)] py-4 max-w-[700px] col-span-2'>
-                    <p>I'm <span className='text-blue-500 text-2xl font-bold'>Bin Li</span>, a Full Stack Developer based in Sydney, 
-                        Australia, specializing in the LAMP stack and currently expanding my skills to include the NERM stack. 
-                        Additionally, I have a strong interest in AI and Big Data technologies.</p>
+                    <p>
+                        I'm <span className='text-blue-500 text-2xl font-bold'>
+                        <Typewriter
+                            options={{
+                                strings: [' Bin Li', ' a Full Stack Developer', ' Practically Glued to AWS ^_^', ' also an AI Enthusiast'],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        />
+                        </span> 
+                    </p>
+                    <p className='py-10'>
+                    My expertise lies in the LAMP stack, and I am currently broadening my skill 
+                    set to include the NERM stack. In addition to my proficiency in web 
+                    development technologies, I have a keen interest in exploring AI and Big Data technologies to create innovative solutions.</p>
                     <Link
                         to="work"
                         smooth={true}
                         duration={500}
                             >
                         <button className='text-[color:var(--primary-text-color-normal)] group border-2 px-6 py-3 my-10 flex items-center hover:bg-blue-500'>
-                            View Work 
+                        Peek at My Adventures 
                             <span className='group-hover:rotate-90 duration-300'>
                                 <HiArrowNarrowRight className='ml-3 ' />
                             </span>
